2b411f9ccab5630a73743ade82c0e6a0f41e6aeb
Justus Dieckmann <justusdieckmann@wwu.de>
2023-05-30 17:39:42 +0200

    Add setting for specifying mail recipients

--- lib.php
+++ lib.php
@@ -118,16 +118,39 @@ class adminapprove extends libbase {
      * @throws \coding_exception
      */
     public function post_processing_bulk_operation() {
-        global $CFG;
+        global $CFG, $DB;
         if (self::$newcourses > 0) {
             $obj = new \stdClass();
             $obj->amount = self::$newcourses;
             $obj->url = $CFG->wwwroot . '/admin/tool/lifecycle/step/adminapprove/index.php';
 
-            email_to_user(get_admin(), \core_user::get_noreply_user(),
-                get_string('emailsubject', 'lifecyclestep_adminapprove'),
-                get_string('emailcontent', 'lifecyclestep_adminapprove',  $obj),
-                get_string('emailcontenthtml', 'lifecyclestep_adminapprove', $obj));
+            $mailusers = [get_admin()];
+            if ($configmailusers = get_config('lifecyclestep_adminapprove', 'mailusers')) {
+                $mailusers = [];
+                $userids = preg_split("/[\s,;]/", $configmailusers);
+                foreach ($userids as $userid) {
+                    if (str_starts_with($userid, '0b')) {
+                        $userid = intval(substr($userid, 2), 2);
+                    } else if (str_starts_with($userid, '0x')) {
+                        $userid = intval(substr($userid, 2), 16);
+                    } else {
+                        $userid = intval($userid);
+                    }
+                    if ($userid) {
+                        $user = $DB->get_record('user', ['id' => $userid]);
+                        if ($user) {
+                            $mailusers[] = $user;
+                        }
+                    }
+                }
+            }
+
+            foreach ($mailusers as $mailuser) {
+                email_to_user($mailuser, \core_user::get_noreply_user(),
+                    get_string('emailsubject', 'lifecyclestep_adminapprove'),
+                    get_string('emailcontent', 'lifecyclestep_adminapprove',  $obj),
+                    get_string('emailcontenthtml', 'lifecyclestep_adminapprove', $obj));
+            }
         }
     }
 
